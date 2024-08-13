3ddac0d6e4e8f2acee93b5e1cba9c2d22118e7e7
tanplusblue <125233727+tanplusblue@users.noreply.github.com>
2024-08-13 14:16:04 -0700

    Update clip.js

--- clip.js
+++ clip.js
@@ -12,3 +12,20 @@ function myFunction() {
   // Alert the copied text
   alert("Copied the text: " + copyText.value);
 }
+
+
+
+function myFunction2() {
+  // Get the text field
+  var copyText = document.getElementById("myInput2");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
